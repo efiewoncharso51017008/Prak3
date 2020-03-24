a6924f6edbde48f623f491e531b034b4bad6a1eb
efiewoncharso51017008 <48231310+efiewoncharso51017008@users.noreply.github.com>
2020-03-24 15:19:00 +0700

    ada perubahan
    hello word jdi nama dan nim

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Efie Woncharso 51017008 Sistem Informasi 2017 \n')
 })
 
 app.listen(PORT, HOST)
